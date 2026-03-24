26d57b1a8147ff172d5a45d7461e32e0fb362efd
Li-Fengl <98276349+Li-Fengl@users.noreply.github.com>
2026-03-24 19:31:17 +0800

    Fix cron job entry for ntpdate command

--- agent.sh
+++ agent.sh
@@ -165,7 +165,7 @@ sync_time(){
     elif check_sys sysRelease centos; then
         yum -y install ntpdate wget
         /usr/sbin/ntpdate -u pool.ntp.org || true
-        ! grep -q "/usr/sbin/ntpdate -u pool.ntp.org" /var/spool/cron/root > /dev/null 2>&1 && echo '0 0 * * * /usr/sbin/ntpdate -u pool.ntp.org > /dev/null 2>&1" )' >> /var/spool/cron/root
+        ! grep -q "/usr/sbin/ntpdate -u pool.ntp.org" /var/spool/cron/root > /dev/null 2>&1 && echo '0 0 * * * /usr/sbin/ntpdate -u pool.ntp.org > /dev/null 2>&1' >> /var/spool/cron/root
         service crond restart
     fi
 
